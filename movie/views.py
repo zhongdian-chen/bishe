549dad218f06044285a87a73ef171e82d9c289aa
zhongdian-chen <1195401008@qq.com>
2019-12-31 17:24:30 +0800

    1

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def login(request):
-    return render(request, 'login.html')
 
 def welcome(request):
     return render(request, 'welcome.html')
